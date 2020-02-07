578fbe9a5d24ad251eca6a9deeeeb33077e3998f
bback462 <60359318+bback462@users.noreply.github.com>
2020-02-07 12:06:32 -0500

    admin test

--- web/admin.php
+++ web/admin.php
@@ -34,51 +34,26 @@ if ($_SESSION['id'] == 999){
 
 }
 else {
-  echo "not admin";
-
-
-
-
-
-//  header('Location: /login');
+    header('Location: /');
+    exit();
 }
 
- /*<!DOCTYPE html>
 <html>
-<head>
-    <?php require_once("./views/header.html"); ?>
-</head>
-
+<head><?php require_once("./views/header.html"); ?></head>
 
 <body>
 <?php require_once("./views/nav.html") ?>;
 
 <html xmlns:th="http://www.thymeleaf.org" th:replace="~{fragments/layout :: layout (~{::body},'profile')}">
 <body>
-  <div class="jumbotron text-center">
-    <div class="container">
-      <h1>Bear Link</h1>
 
 
 
-<?php
-$conn = pg_connect(getenv("DATABASE_URL"));
+<form class="loginform" method="post" action="adminDeleteUser.php"
+    <br><label>Enter User ID to DELETE</label><br>
+    <input type="deletedID" name="deletedID" placeholder="Enter User ID"><br>
+
+    </html>
 
 
-session_start();
-if (!isset($_SESSION['id'])){
-    echo "not logged in";
-}
-
-echo "<p>Logged in as: {$_SESSION['id']}</p>";
-//get user info
-
-if(!(array_key_exists('id', $_GET))){
-    $_GET['id'] = $_SESSION['id'];
-}
-/*
-$result = pg_prepare($conn, "fetchRequestedUser", 'SELECT * FROM users WHERE id = $1');
-$uid = (int)$_GET['id'];
-$result = pg_execute($conn, "fetchRequestedUser", array($uid));
-*/
 ?>
